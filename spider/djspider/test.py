023e7a6324fe4e5e7275f31f83195723e1536087
prodja <prodjango@yandex.ru>
2016-01-18 17:27:46 +0300

    add link api elastic python

--- spider/djspider/test.py
+++ spider/djspider/test.py
@@ -4,6 +4,7 @@ import httplib2
 from os import getcwd
 #test elastic in python
 #товар - распарсил, в json -> (если _source пуст)документ elastic - заполняем _source в доке
+#https://elasticsearch-py.readthedocs.org/en/master/api.html
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 doc='{"name":"one_name", "arrs":{"el1":1,"els2":2}}'
 res = es.index(index="gearbest_index", doc_type='product_type', id=5, body=doc)
