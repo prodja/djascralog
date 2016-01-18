e89e543284335e046fc5cec9d8d5dc6fa567e732
prodja <prodjango@yandex.ru>
2016-01-18 14:31:36 +0300

    del 1

--- spider/djspider/test.py
+++ spider/djspider/test.py
@@ -3,6 +3,7 @@ from elasticsearch import Elasticsearch
 import httplib2
 from os import getcwd
 #test elastic in python
+#товар - распарсил, в json -> (если _source пуст)документ elastic - заполняем _source в доке
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 doc='{"name":"one_name", "arrs":{"el1":1,"els2":2}}'
 res = es.index(index="gearbest_index", doc_type='product_type', id=5, body=doc)
