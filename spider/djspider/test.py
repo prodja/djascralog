5639e2963bc707e3c10e13ae92ca437bca2d5fa7
prodja <prodjango@yandex.ru>
2016-01-18 12:51:05 +0300

    first in job

--- spider/djspider/test.py
+++ spider/djspider/test.py
@@ -2,7 +2,7 @@
 from elasticsearch import Elasticsearch
 import httplib2
 from os import getcwd
-
+#test elastic in python
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 doc='{"name":"one_name", "arrs":{"el1":1,"els2":2}}'
 res = es.index(index="gearbest_index", doc_type='product_type', id=5, body=doc)
