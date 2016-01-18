109406effaad71e2863a0a94dfb643a31a7ab9b3
prodja <prodjango@yandex.ru>
2016-01-18 17:57:22 +0300

    can t exists

--- spider/djspider/test.py
+++ spider/djspider/test.py
@@ -7,4 +7,8 @@ from os import getcwd
 #https://elasticsearch-py.readthedocs.org/en/master/api.html
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 doc='{"name":"one_name", "arrs":{"el1":1,"els2":2}}'
-res = es.index(index="gearbest_index", doc_type='product_type', id=5, body=doc)
+#res = es.index(index="gearbest_index", doc_type='product_type', id=5, body=doc)
+#res=es.create(index="gearbest_index",doc_type='product_type',body=doc,id=1)
+b=es.exists(index="gearbest_index",doc_type="product_type",id=1)
+if(b==True):
+	print 'OK'
