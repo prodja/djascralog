b9587aa567fece459aa912cb2d3e057ff1509343
prodja <prodjango@yandex.ru>
2016-01-22 17:50:48 +0300

    parse_ok

--- spider/djspider/djspider/pipelines.py
+++ spider/djspider/djspider/pipelines.py
@@ -14,24 +14,19 @@ class DjspiderPipeline(object):
       if(es.exists(index="gearbest_index",doc_type='product_type',id=item['code'])):
         print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         print str(item['code'])+' - exist'
-
         get_doc=es.get(index="gearbest_index",doc_type='product_type',id=item['code'],_source=True)
         list_of_fieds=['price_reg','price_discount']
         for lf in list_of_fieds:
-          if lf in item and len(item[lf])>0:
-            #import pdb; pdb.set_trace()
-            if lf in get_doc['_source']:
-              print  str(item['code']) + '*==*' + str(item[lf])
-              print 'sc=' + str(get_doc['_source'])
-              get_doc['_source'][lf]=list(get_doc['_source'][lf])
-              #item[lf]=
-              #i=type(get_doc['_source'][lf])
-              import pdb; pdb.set_trace()
-              new_list=list(get_doc['_source'][lf])
-
-              i=new_list.append(str(item[lf]))
-
-              print 'LLLLLLLLLLLLLLLLLLLLLLLLLLIiiiiist='+str(i)
+          if lf in item and len(item[lf])>0 and lf in get_doc['_source']:
+            last_item=item[lf][0]
+            #item[lf]=[]
+            try:
+              if len(get_doc['_source'][lf])>0:
+                get_doc['_source'][lf].append(last_item)
+                item[lf]=get_doc['_source'][lf]
+                print '(OK_OK_OK_OK_OK_OK_OK_OK_OK_OK)='+item['code']+' '+lf
+            except TypeError:
+                print '()()(()()()()(()()()TypeError='+item['code']
 
       item = dict(item)        
       es.index(index="gearbest_index",doc_type='product_type', body=item, id=item['code'])
