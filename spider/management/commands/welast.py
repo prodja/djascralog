ecc0821e1bc23c9e7fd3a3edea453168664c2a7f
konstantin <288825@gmail.com>
2016-01-29 19:28:25 +0300

    f

--- spider/management/commands/welast.py
+++ spider/management/commands/welast.py
@@ -23,27 +23,6 @@ class Command(BaseCommand):
 
 		for jsonline in fop:
 			jobj=loads(jsonline)
-			jobj.pop("_type")
-			import pdb; pdb.set_trace()
-			if(es.exists(index="gearbest_index",doc_type='product_type',id=jobj['code'])):
-				get_doc=es.get(index="gearbest_index",doc_type='product_type',id=jobj['code'],_source=True)
-				list_of_fieds=['price_reg','price_discount']
-				for lf in list_of_fieds:
-					if lf in jobj and len(jobj[lf])>0:
-						last_item=unicode(jobj[lf][0])
-						try:
-							if len(get_doc['_source'][lf])>0:
-								nl=[]
-							if(type(get_doc['_source'][lf])==list):
-								nl=get_doc['_source'][lf]
-							elif(type(get_doc['_source'][lf])==unicode):
-								nl.append(get_doc['_source'][lf]) 
-							nl.append(last_item)
-							jobj[lf]=nl
-						except TypeError:
-							import pdb; pdb.set_trace()
-							print '()()(()()()()(()()()TypeError='+jobj['code']
-
-			jobj = dict(jobj)        
+			del jobj["_type"]
 			es.index(index="gearbest_index",doc_type='product_type', body=jobj, id=jobj['code'])
 
