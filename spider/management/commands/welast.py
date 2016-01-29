2a8762dea18a9bd469582536137ec87c54ebc504
prodja <prodjango@yandex.ru>
2016-01-29 18:43:50 +0300

    dgfs

--- spider/management/commands/welast.py
+++ spider/management/commands/welast.py
@@ -1,6 +1,7 @@
 #! coding=utf-8
 from django.core.management.base import BaseCommand
 from elasticsearch import Elasticsearch
+from elasticsearch.client import IndicesClient
 from json import loads 
 import re
 #import pdb; pdb.set_trace()
@@ -10,13 +11,22 @@ class Command(BaseCommand):
 
 		es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
 		fop=open('spider/management/commands/items_gearbest_8.jl', 'r')
+		inds = IndicesClient(es)
+
+		mapping='{ "mappings": { "product_type":  {  "properties": { "code": { "type" : "string" },"name": {"type" : "string"},"img": {"type" : "string"},"url": {"type" : "string"},"price_reg": {"type" : "float"},"price_discount": {"type" : "float"}}}}}'
+
+		if(inds.exists(index='gearbest_index')):
+			print 'gearbest_index exist'
+		else:
+			inds.create(index='gearbest_index',body=mapping)
+			print 'gearbest_index created'
 
 		for jsonline in fop:
 			jobj=loads(jsonline)
 			jobj.pop("_type")
+			import pdb; pdb.set_trace()
 			if(es.exists(index="gearbest_index",doc_type='product_type',id=jobj['code'])):
 				get_doc=es.get(index="gearbest_index",doc_type='product_type',id=jobj['code'],_source=True)
-				list_of_fieds=['price_reg','price_discount']
 				list_of_fieds=['price_reg','price_discount']
 				for lf in list_of_fieds:
 					if lf in jobj and len(jobj[lf])>0:
@@ -33,5 +43,7 @@ class Command(BaseCommand):
 						except TypeError:
 							import pdb; pdb.set_trace()
 							print '()()(()()()()(()()()TypeError='+jobj['code']
+
 			jobj = dict(jobj)        
 			es.index(index="gearbest_index",doc_type='product_type', body=jobj, id=jobj['code'])
+
