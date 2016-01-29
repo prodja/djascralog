364d065a3b80d2b3109f5641fda512e4681d1145
prodja <prodjango@yandex.ru>
2016-01-29 14:32:25 +0300

    ok 1

--- spider/management/commands/welast.py
+++ spider/management/commands/welast.py
@@ -9,15 +9,11 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 
 		es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
-		fop=open('spider/management/commands/items.txt', 'r')
-		r=str(fop.read())
-		#r="{u'_type': u'GearBestItem', u'code': u'132992601', u'name': u'G1W-CB Full Black 2.7 inch 1080P Full HD Car DVR 5.0MP Resolution 4X Digital Zoom Video Recorder 120 Degree Wide Angle Lens with Charger  (Capacitor Battery)', u'img': u'http://gloimg.gearbest.com/gb/2015/201506/goods-img/1433815949936-P-2714228.jpg', u'_key': u'34190/1/8/0', u'url': u'http://www.gearbest.com/car-dvr/pp_196898.html', u'price_reg': [u'85.63'], u'price_discount': [u'41.64']}"
-		list_jsons=[]
-		r=r.replace("u'", '"')
-		r=r.replace("'", '"')
-		arr_json=re.findall("{.*}",r)
-		for jsonline in arr_json:
+		fop=open('spider/management/commands/items_gearbest_8.jl', 'r')
+
+		for jsonline in fop:
 			jobj=loads(jsonline)
+			jobj.pop("_type")
 			if(es.exists(index="gearbest_index",doc_type='product_type',id=jobj['code'])):
 				get_doc=es.get(index="gearbest_index",doc_type='product_type',id=jobj['code'],_source=True)
 				list_of_fieds=['price_reg','price_discount']
