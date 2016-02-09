9c4fa59f2501fff2410b504b0fac943042b48272
prodja <prodjango@yandex.ru>
2016-02-09 17:58:47 +0300

    pg2

--- spider/fns.py
+++ spider/fns.py
@@ -3,16 +3,21 @@ from elasticsearch import Elasticsearch
 
 def sel_from_elast(find='',fields=[],index_doc='gearbest_index',doctype='product_type',size_s=10,form_s=0):
 
+	"""Запрос в эластик"""
 	es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
 	qdsl={'fields': fields, 'query': {'match': {'_all': find}}}
 	res=es.search(index=index_doc,doc_type=doctype, body=qdsl,size=size_s,from_=form_s)
 	out_dict={}
 
+	"""Вычисляем количество страниц """
 	total=res['hits']['total']
 	tmp=total/size_s
 	out_dict['page_count']=round(tmp+0.5)
+
+	"""и генерируем список страниц"""
 	out_dict['pages']=[x for x in range(1,int(out_dict['page_count']))]
 
+	"""Заполняем результатами выходной словарь"""
 	res=res['hits']['hits']
 	out_dict['finded']=[]
 	for rs in res:
--- spider/views.py
+++ spider/views.py
@@ -1,23 +1,21 @@
 #! coding: utf-8
-from django.shortcuts import render
 from django.template import RequestContext
-from django.shortcuts import render_to_response, redirect
-from re import match
+from django.shortcuts import render_to_response
 from forms import SearchForm
-from django.core.paginator import Paginator
 from fns import sel_from_elast
-#from django.core.files.uploadedfile import UploadedFile
 
 def spider(request,page=1,zap_from_get=''):
 	context={}	
 	context['spider']=1
+	""" кол-во выводимых товаров на одной странице (size)"""
 	hits_count=7
-	#file_data = {'sub': UploadedFile('/static/search.png')}
 
+	""" Если запрос пришел из формы """
 	if request.method == 'POST':
 
 		context['search_form'] = SearchForm(request.POST)
 
+		""" Проверка пришедшего с формы и его подготовка к запросу в эластик """
 		if context['search_form'].is_valid():
 
 			find=context['search_form'].cleaned_data['search']
@@ -30,18 +28,24 @@ def spider(request,page=1,zap_from_get=''):
 
 				context['curpage']=page
 				context['zap_from_get']=find
+
+				""" Запрос в эластик и получение товаров """
 				context=dict(context.items()+sel_from_elast(find,['name', 'code','url'],'gearbest_index','product_type',hits_count,page-1).items())
 			
 			else:
 				context['message']='Введите в поиск корректную строку (более одного символа)'		
 
-	else: 
+	else:
+		""" Если запрос пришел с url'а (с постраничника) """ 
 		context['search_form'] = SearchForm()
+
+		""" Если с постраничника пришел текст запроса с формы """ 
 		if zap_from_get!='':
-			#s={'search':zap_from_get}
-			#file_data=dict(file_data.items()+s.items()) SearchForm(file_data)
+
 			context['search_form'] = SearchForm({'search':zap_from_get})
 			context['zap_from_get']=zap_from_get
+
+			""" Делаем запрос в эластик и получаем товары """
 			context=dict(context.items()+sel_from_elast(zap_from_get,['name', 'code','url'],'gearbest_index','product_type',hits_count,int(page)-1).items())
 			context['search_form'] = SearchForm()
 
